6ee7096165a54d814358b3fabb249bd9c277f3d3
sonalika-samal <sonalika.ctc29@gmail.com>
2024-02-07 18:56:42 +0530

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var menuButton = document.getElementById("menuButton");
+  var mobileMenu = document.getElementById("mobileMenu");
+
+  menuButton.addEventListener("click", function () {
+    // Toggle the .show class on and off
+    mobileMenu.classList.toggle("show");
+  });
+});
+
+// https://i.pinimg.com/736x/cb/fe/6b/cbfe6b3d7a40901be03e73c95ef7f257.jpg
+
+// https://tse3.mm.bing.net/th/id/OIP.lXzAtj_naKmmb21G0pDZbQHaLH?rs=1&pid=ImgDetMain
+
+// https://tse3.mm.bing.net/th/id/OIP.3zK4me02RDyuGBHqk6hlwQAAAA?rs=1&pid=ImgDetMain
+
+// https://media-cdn.tripadvisor.com/media/photo-s/1d/29/7f/41/the-best-cup-of-coffee.jpg
+
+// https://www.meganstarr.com/wp-content/uploads/2019/07/Things-to-do-in-Tallinn-in-Winter-28-735x551.jpg
+
+// https://toppng.com/uploads/preview/coffee-cappuccino-cup-foam-drink-11570149928ymb7cg1bpw.jpg
+
+// https://tse3.mm.bing.net/th/id/OIP.E3_p_twiMbYswHw-UIW9ywHaFj?w=600&h=450&rs=1&pid=ImgDetMain
+
+// https://th.bing.com/th/id/R.6f783b0290d7076ecda3d98eab875183?rik=ez%2fagFSnlGZPLQ&riu=http%3a%2f%2ffrugalfrolicker.com%2fwp-content%2fuploads%2f2015%2f05%2fwpid10383-sydney-coffee.jpg&ehk=pMy9H6X3VIv7vRVo00vhvofLeG2uz9vgyEvRGdDpnN8%3d&risl=&pid=ImgRaw&r=0
+
+// https://tse3.mm.bing.net/th/id/OIP.AtGqUewWUSJnzU7CNyMzPAHaHa?w=500&h=500&rs=1&pid=ImgDetMain
